73bd262ef23c97c46b27595cc1f9e5e88ccc432d
Константин Павленко <romof1977@gmail.com>
2023-09-30 10:44:22 +0300

    Добавил Assert

--- timer.pas
+++ timer.pas
@@ -1,4 +1,7 @@
 ﻿begin
   var (h, m, s) := ReadInteger3('Введите кол-во часов, минут и секунд:');
+  Assert(h > 0);
+  Assert(m in 0..59);
+  Assert(s in 0..59);
   Println($'Суммарное кол-во секунд: {h*3600+m*60+s}');
 end.
