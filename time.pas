a6345e9bde6a4b8bde07afa574481cbd8a495589
Константин Павленко <romof1977@gmail.com>
2023-09-30 10:33:28 +0300

    Изменил time.pas чтобы печатало кол-во дней в году и добавил timer.pas

--- time.pas
+++ time.pas
@@ -1,4 +1,7 @@
 ﻿begin
   var year := ReadInteger('Введите год:');
-  Println($'Этот год високосный? - {year.Divs(4) and (not year.Divs(100) or year.Divs(400))}');
+  if year.Divs(4) and (not year.Divs(100) or year.Divs(400)) then
+    Println('В этом году 366 дней')
+  else
+    Println('В этом году 365 дней');
 end.
