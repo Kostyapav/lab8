c118766124b57c7e13781321223d740b99b2ec10
Константин Павленко <romof1977@gmail.com>
2023-09-30 10:12:41 +0300

    Time of day

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  print('Привет, мир!');
+  var hour := ReadInteger('Введите текущий час:');
+  Assert(hour in 0..23);
+  case hour of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вечер, мир!');
+    else Println('Доброй ночи, мир!');
+  end;
 end.
